12b5145344c2752aa01990cbbfc6b2bf2db8986f
Snackhole <35538327+Snackhole@users.noreply.github.com>
2018-03-21 14:28:48 -0500

    Update version number in main class

--- src/main/java/com/snackhole/simplechunkloader/SimpleChunkloaderMain.java
+++ src/main/java/com/snackhole/simplechunkloader/SimpleChunkloaderMain.java
@@ -15,7 +15,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = SimpleChunkloaderMain.MODID, version = SimpleChunkloaderMain.VERSION)
 public class SimpleChunkloaderMain {
     public static final String MODID = "simplechunkloader";
-    public static final String VERSION = "1";
+    public static final String VERSION = "2";
     @SidedProxy(clientSide = "com.snackhole.simplechunkloader.proxy.ClientProxy", serverSide = "com.snackhole.simplechunkloader.proxy.ServerProxy")
     public static IProxy proxy;
     @Mod.Instance
